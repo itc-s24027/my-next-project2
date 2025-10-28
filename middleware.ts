3e2a944bd4aa58fe58bfc7148a5fe6fbc36ac14a
itc-s24027 <s24027@std.it-college.ac.jp>
2025-10-28 12:19:15 +0900

    8章まで完了

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "domain";
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware();
-
-export const config = {
-    matcher: ["/(.*)"],
-};
-//matcherを指定しない場合、すべてのリクエストに対してmiddlewareが実行される
